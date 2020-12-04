26d5cdc5de48fc938828bd3c2144df678e8180e8
Ocean <55033382+TheOceanPony@users.noreply.github.com>
2020-12-05 01:06:53 +0200

    Update funcs2.py

--- funcs2.py
+++ funcs2.py
@@ -7,6 +7,7 @@ def init_unary(maxD, imL, imR, c):
     height, width = imL.shape
     H = np.zeros((height, width, maxD + 1), dtype=np.float32)
 
+    print("H initialising...")
     for j in range(0, height):
         for i in range(0, width):
             for d in range(0, maxD + 1):
@@ -26,37 +27,39 @@ def init_binary(maxD, alpha, c):
     return G
 
 
-#@njit
+@njit
 def init_left(H, G):
 
     height, width, maxD  = H.shape[0], H.shape[1], H.shape[2] - 1
     Li = np.zeros((height, width, maxD + 1, width))
 
     print(f"Li initialising...")
-    for i in tqdm( range(0, height) ):
+    for i in range(0, height):
         for j in range(0, width):
             for d in range(0, maxD):
-                Li[i, j, d, :] = left(i, j ,d, Li, H, G)
 
+                Li[i, j, d] = left(i, j ,d, Li, H, G, Li[i, j, d])
 
-#@njit
-def left(i, j, d, Li, H, G):
+    return Li
+
+@njit
+def left(i, j, d, Li, H, G, res):
 
     height, width, maxD = H.shape[0], H.shape[1], H.shape[2] - 1
 
     if j == 0:
-        return 0
+        return np.zeros(width)
     else:
-        res = np.zeros(width)
+        temp = np.zeros(width)
 
         for _d in range(0, maxD+1):
-            temp = np.zeros(width)
+
             temp[j-1] = _d
             res += H[i, j, _d]*G[d, _d]*temp + Li[i, j-1, _d]
 
         return res
 
-    
+
 @njit
 def init_right(H, G):
     height, width, maxD = H.shape[0], H.shape[1], H.shape[2] - 1
@@ -68,6 +71,7 @@ def init_right(H, G):
             for d in range(0, maxD):
                 Ri[i, j, d] = right(i, j, d, Ri, H, G, Ri[i, j, d])
 
+    return Ri
 
 @njit
 def right(i, j, d, Ri, H, G, res):
@@ -83,3 +87,5 @@ def right(i, j, d, Ri, H, G, res):
             res += H[i, j, _d] * G[d, _d] * temp + Ri[i, j + 1, _d]
 
         return res
+
+
