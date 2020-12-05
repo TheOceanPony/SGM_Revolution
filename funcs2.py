45f8112b93d75cf6cde61045b8ccaf7a87d7122a
Ocean <55033382+TheOceanPony@users.noreply.github.com>
2020-12-05 23:55:59 +0200

    Redone

--- funcs2.py
+++ funcs2.py
@@ -1,91 +1,42 @@
 import numpy as np
 from numba import njit
 
-@njit
-def init_unary(maxD, imL, imR, c):
 
-    height, width = imL.shape
-    H = np.zeros((height, width, maxD + 1), dtype=np.float32)
+#@njit
+def magic(img_L, img_R, row, MAX_DISP):
 
-    print("H initialising...")
-    for j in range(0, height):
-        for i in range(0, width):
-            for d in range(0, maxD + 1):
-                H[j, i, d] = np.exp(c - abs(imL[j, i] - imR[j, i - d]))
-    return H
+    width = img_L.shape[1]
 
+    F = np.zeros((MAX_DISP + 1, width))
+    for iteration in range(1, width):
+        F[:, -iteration] = np.arange(0, MAX_DISP + 1)
+        F2 = F.copy()
 
-@njit
-def init_binary(maxD, alpha, c):
+        for t in range(1, iteration + 1):
+            for k in range(0, MAX_DISP + 1):
+                F2[k, width - t] = np.average(F[:, width - t], weights=p2(k, MAX_DISP, row, width - iteration, img_L, img_R))
 
-    G = np.zeros((maxD + 1, maxD + 1), dtype=np.float32)
+        F = F2.copy()
 
-    for di in range(0, maxD + 1):
-        for dj in range(0, maxD + 1):
-            G[di, dj] = np.exp(c - alpha * abs(di - dj))
+    res = np.zeros(width)
+    for k1 in range(0, MAX_DISP + 1):
+        res += (1 / (MAX_DISP + 1)) * F[k1]
 
-    return G
+    return res
 
 
 @njit
-def init_left(H, G):
+def p2(k, maxD, i, j, imL, imR, alpha = 0.1, c=0):
+    w= np.ones(maxD+1)
+    for _k in range(0, maxD+1):
+        w[_k] = np.exp(c - alpha*( k - _k )) * np.exp(c - abs(imL[i, j] - imR[i, j - _k]))
 
-    height, width, maxD  = H.shape[0], H.shape[1], H.shape[2] - 1
-    Li = np.zeros((height, width, maxD + 1, width))
-
-    print(f"Li initialising...")
-    for i in range(0, height):
-        for j in range(0, width):
-            for d in range(0, maxD):
-
-                Li[i, j, d] = left(i, j ,d, Li, H, G, Li[i, j, d])
-
-    return Li
-
-@njit
-def left(i, j, d, Li, H, G, res):
-
-    height, width, maxD = H.shape[0], H.shape[1], H.shape[2] - 1
-
-    if j == 0:
-        return np.zeros(width)
-    else:
-        temp = np.zeros(width)
-
-        for _d in range(0, maxD+1):
-
-            temp[j-1] = _d
-            res += H[i, j, _d]*G[d, _d]*temp + Li[i, j-1, _d]
-
-        return res
+    return w/np.sum(w)
 
 
-@njit
-def init_right(H, G):
-    height, width, maxD = H.shape[0], H.shape[1], H.shape[2] - 1
-    Ri = np.zeros((height, width, maxD + 1, width))
 
-    print(f"Ri initialising...")
-    for i in range(0, height):
-        for j in range(0, width):
-            for d in range(0, maxD):
-                Ri[i, j, d] = right(i, j, d, Ri, H, G, Ri[i, j, d])
 
-    return Ri
 
-@njit
-def right(i, j, d, Ri, H, G, res):
-    height, width, maxD = H.shape[0], H.shape[1], H.shape[2] - 1
 
-    if j == width:
-        return np.zeros(width)
-    else:
-        temp = np.zeros(width)
-
-        for _d in range(0, maxD + 1):
-            temp[j + 1] = _d
-            res += H[i, j, _d] * G[d, _d] * temp + Ri[i, j + 1, _d]
-
-        return res
 
 
