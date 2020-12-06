fbe2c73a9e1b7df65ae74008742c302ed653b111
Ocean <55033382+TheOceanPony@users.noreply.github.com>
2020-12-06 16:20:06 +0200

    Lookup table

--- main2.py
+++ main2.py
@@ -5,26 +5,32 @@ from skimage.color import rgb2gray
 
 import funcs2 as f2
 
+
+
 if __name__ == '__main__':
 
     # Params
-    MAX_DISP = 5
-    alpha = 0.1
+    maxD = 5
+    alpha = 1
+    beta = 100
     C = 0
 
     # Reading imgs
-    img_L = (rgb2gray( imread("imgs/im0.png")[100:200, 100:200]))
-    img_R = (rgb2gray( imread("imgs/im1.png")[100:200, 100:200]))
+    img_L = (rgb2gray( (imread("imgs/im0.png")) ))
+    img_R = (rgb2gray( (imread("imgs/im1.png")) ))
 
     height, width = img_L.shape
     print(f"Img info - shape: {width, height}, max el: {np.max(img_L)}, dtype: {img_L.dtype}")
 
-    Dm = np.zeros((height, width))
-    for row in tqdm(range(0, height)):
-        Dm[row, :] = f2.magic(img_L, img_R, row, MAX_DISP)
+    G = f2.G_init(maxD, alpha, C)
+    H = f2.H_init(img_L, img_R, maxD, beta, C)
 
-    Dm = Dm * (np.max(Dm) / MAX_DISP)
-    print(np.max(Dm), Dm.shape)
-    imsave("imgs/out/result.png", Dm)
-    imsave("imgs/out/L.png", img_L)
-    imsave("imgs/out/R.png", img_R)
+    P = f2.P_init(H, G)
+
+    Dm = np.zeros((height, width), dtype=np.float64)
+    for row in tqdm(range(0, height)):
+        Dm[row, :] = f2.magic(row, width, maxD, P)
+
+    # Dm = Dm * (np.max(Dm) / maxD)
+    print(np.max(Dm), np.min(Dm), Dm.shape)
+    imsave("imgs/out/result_a2.png", Dm)
